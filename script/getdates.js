7839fda9dccf77f36fea8d6c0748b95604c842ee
Fx_Apostle <ilo23002@byui.edu>
2025-07-11 22:09:39 +0100

    correction comes

--- script/getdates.js
+++
@@ -1,7 +0,0 @@
-// Set the copyright year
-const currentYear = new Date.getFullYear();
-document.getElementById('year').textContent = currentYear;
-
-// Set the Last modified date
-const lastModified = document.lastModified;
-document.getElementById('lastModified').textContent = 'lastModified: ' + lastModified;
