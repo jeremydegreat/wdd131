e9d833d11d7ae220e724645c90ae93d04f62a1b3
Fx_Apostle <ilo23002@byui.edu>
2025-10-02 07:17:12 +0100

    adding favicon

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,19 @@
+
+// Set the Last modified date
+const lastModified = document.lastModified;
+document.getElementById("getdate").textContent = 'last Modified: ' + lastModified;
+
+// Toggle function
+const hamburger = document.getElementById("hamburger");
+const navlinks = document.querySelector(".navlinks");
+
+hamburger.addEventListener("click", () => {
+  navlinks.classList.toggle("active");
+
+  // change button symbol
+  if (hamburger.textContent === "☰") {
+    hamburger.textContent = "✖"; // X
+  } else {
+    hamburger.textContent = "☰"; // back to bars
+  }
+});
